b800465eb8da802843d7e61fb153717fc5595723
swalekhan <swalekhan1522@gmail.com>
2022-12-28 13:39:11 +0530

    async-and-error-handle

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,70 +1,84 @@
 import React, { useState } from "react";
 import { Button, Navbar, Table } from "react-bootstrap";
 import MoviesItem from "./MoviesItem";
+import Spiner from "./Spiner";
 const Home = () => {
-
+  const [error, setError] = useState()
+  const [spiner, setSpiner] = useState(false)
   const [state, setState] = useState([]);
 
-  const fetchHandler =() =>{
-    fetch("https://swapi.dev/api/films/").then((response) =>{
-      return response.json();
-    }).then((data)=>{
-      // const updatedItem = data.results.map((e)=>{
-      // return {
-      //   id:e.episode_id,
-      //   openingText:e.opening_crawl,
-      //   title:e.title,
-      //   releaseData:e.release_date
-      // }
-      // })
-      setState(data.results);
-    })
-  } 
+  const fetchHandler = async () => {
+    setSpiner(true)
+    try {
+      let response = await fetch("https://swapi.dev/api/films/")
 
-return (
+      if (!response.ok) {
+        throw new Error("somthing wrong with this")
+      }
+
+      let data = await response.json()
+      const updatedItem = data.results.map((e) => {
+        return {
+          id: e.episode_id,
+          openingText: e.opening_crawl,
+          title: e.title,
+          releaseData: e.release_date
+        }
+      })
+      setState(updatedItem);
+    } catch (error) {
+      setError(error.message)
+    }
+    setSpiner(false)
+  }
+
+  return (
     <>
-    <h1 style={{textAlign:"center", margin:"15px"}}>TOUR</h1>
-    <Table>
-    <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td><Button variant="primary">BUY TICKET</Button></td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td><Button variant="primary">BUY TICKET</Button></td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td >Larry the Bird</td>
-          <td >Larry the Bird</td>
-          <td><Button variant="primary">BUY TICKET</Button></td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td >Larry the Bird</td>
-          <td >Larry the Bird</td>
-          <td><Button variant="primary">BUY TICKET</Button></td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td >Larry the Bird</td>
-          <td >Larry the Bird</td>
-          <td><Button variant="primary">BUY TICKET</Button></td>
-        </tr>
-      </tbody>
+      <h1 style={{ textAlign: "center", margin: "15px" }}>TOUR</h1>
+      <Table>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td><Button variant="primary">BUY TICKET</Button></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td><Button variant="primary">BUY TICKET</Button></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td >Larry the Bird</td>
+            <td >Larry the Bird</td>
+            <td><Button variant="primary">BUY TICKET</Button></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td >Larry the Bird</td>
+            <td >Larry the Bird</td>
+            <td><Button variant="primary">BUY TICKET</Button></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td >Larry the Bird</td>
+            <td >Larry the Bird</td>
+            <td><Button variant="primary">BUY TICKET</Button></td>
+          </tr>
+        </tbody>
       </Table>
-      <Button variant="warning" onClick={fetchHandler}>fetch Movies</Button>
-      {state.map((e)=>(
-        <MoviesItem title = {e.title}  openingText={e.opening_crawl} releaseData = {e.release_date} key={e.episode_id}/>
+      {spiner && <Spiner />}
+      {!spiner && <Button variant="warning" className="m-3 ms-5" onClick={fetchHandler}>fetch Movies</Button>}
+      {!spiner && state.length>0 && state.map((e) => (
+        <MoviesItem title={e.title} openingText={e.opening_crawl} releaseData={e.release_date} key={e.episode_id} />
       ))}
+      {!spiner && state.length === 0 && !error && <p>no Movies found</p>}
+      {!spiner &&  error && <p>{error}</p>}
       <Navbar bg="dark" expand="lg" variant='dark' className="p-5" > <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand></Navbar>
     </>
-)
+  )
 }
 
 export default Home;
---
+++ src/Home/Spiner.js
@@ -0,0 +1,13 @@
+import Loading from './Loading.gif';
+
+const Spiner = () => {
+ return (
+     <>
+         <div className="text-center">
+             <img src={Loading} alt="loading"></img>
+         </div>
+     </>
+ )
+}
+
+export default Spiner;
